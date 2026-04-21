5cd179d039b1bde7b4a71bc44497eafdf80f6a4d
Robert <robert@johannap.se>
2026-04-21 14:46:41 +0200

    fix: Blazor WebAssembly build issues - add missing package and fix Program.cs

--- src/LifeOrchestration.Web/Program.cs
+++ src/LifeOrchestration.Web/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using LifeOrchestration.Web;
-using LifeOrchestration.Web.Pages;
+using LifeOrchestration.Web.Components;
 
-var builder = WebAssemblyBlazorWebAssemblyHost.CreateDefaultBuilder();
+var builder = WebAssemblyHostBuilder.CreateDefault([]);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.AddJavaScriptInitializers("./framework/initBlazor.js");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://192.168.1.194:3080") });
-
 await builder.Build().RunAsync();
